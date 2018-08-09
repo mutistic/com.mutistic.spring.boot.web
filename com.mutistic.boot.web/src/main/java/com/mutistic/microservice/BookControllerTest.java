9cda32149eace457b6d1553563b8ce019171cd25
mutistic-client <mutistic@qq.com>
2018-08-09 16:59:44 +0800

    暂时不引用com.mutistic.spring.boot.server项目

--- com.mutistic.boot.web/src/main/java/com/mutistic/microservice/BookControllerTest.java
+++ com.mutistic.boot.web/src/main/java/com/mutistic/microservice/BookControllerTest.java
@@ -1,52 +1,55 @@
 package com.mutistic.microservice;
 
-import java.util.List;
+public class BookControllerTest {}
 
-import org.springframework.web.client.RestTemplate;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.mutistic.entity.BookEntity;
-import com.mutistic.utils.CommonUtil;
-
-/**
- * @program 演示微服务的使用。使用 RestTemplate 测试调用Controller接口
- * @description 
- * @author mutisitic
- * @date 2018年8月6日
- */
-public class BookControllerTest {
-
-	public static void main(String[] args) {
-		RestTemplate rt = new RestTemplate();
-		testQueryById(rt);
-		testQueryAllList(rt);
-	}
-
-	private static void testQueryAllList(RestTemplate rt) {
-		String responseJSON = rt.getForObject("http://127.0.0.1:8888/bookController/queryAllList",
-				String.class);
-
-		CommonUtil.printOne("获取RestTemplate请求的结果：queryAllList：" + responseJSON);
-		JSONObject resp = (JSONObject) JSON.parse(responseJSON);
-		if (resp.get("data") != null) {
-			List<JSONObject> dataList = JSON.toJavaObject((JSON) resp.parse(resp.get("data").toString()), List.class);
-			for(JSONObject json : dataList) {
-				CommonUtil.printThree("获取JSONObject转成的实体对象：", JSON.toJavaObject((JSONObject)JSON.parse(json.toString()), BookEntity.class));
-			}
-		}
-	}
-
-	private static void testQueryById(RestTemplate rt) {
-		String responseJSON = rt.getForObject("http://127.0.0.1:8888/bookController/queryById?id=1533384516863",
-				String.class);
-
-		CommonUtil.printOne("获取RestTemplate请求的结果：queryById：" + responseJSON);
-		JSONObject resp = (JSONObject) JSON.parse(responseJSON);
-		if (resp.get("data") != null) {
-			BookEntity book = JSON.toJavaObject((JSON) resp.parse(resp.get("data").toString()), BookEntity.class);
-			CommonUtil.printThree("获取JSONObject转成的实体对象：", book.toString());
-		}
-	}
-
-}
+//import java.util.List;
+//
+//import org.springframework.web.client.RestTemplate;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.JSONObject;
+//import com.mutistic.entity.BookEntity;
+//import com.mutistic.utils.CommonUtil;
+//
+//
+///**
+// * @program 演示微服务的使用。使用 RestTemplate 测试调用Controller接口
+// * @description 
+// * @author mutisitic
+// * @date 2018年8月6日
+// */
+//public class BookControllerTest {
+//
+//	public static void main(String[] args) {
+//		RestTemplate rt = new RestTemplate();
+//		testQueryById(rt);
+//		testQueryAllList(rt);
+//	}
+//
+//	private static void testQueryAllList(RestTemplate rt) {
+//		String responseJSON = rt.getForObject("http://127.0.0.1:8888/bookController/queryAllList",
+//				String.class);
+//
+//		CommonUtil.printOne("获取RestTemplate请求的结果：queryAllList：" + responseJSON);
+//		JSONObject resp = (JSONObject) JSON.parse(responseJSON);
+//		if (resp.get("data") != null) {
+//			List<JSONObject> dataList = JSON.toJavaObject((JSON) resp.parse(resp.get("data").toString()), List.class);
+//			for(JSONObject json : dataList) {
+//				CommonUtil.printThree("获取JSONObject转成的实体对象：", JSON.toJavaObject((JSONObject)JSON.parse(json.toString()), BookEntity.class));
+//			}
+//		}
+//	}
+//
+//	private static void testQueryById(RestTemplate rt) {
+//		String responseJSON = rt.getForObject("http://127.0.0.1:8888/bookController/queryById?id=1533384516863",
+//				String.class);
+//
+//		CommonUtil.printOne("获取RestTemplate请求的结果：queryById：" + responseJSON);
+//		JSONObject resp = (JSONObject) JSON.parse(responseJSON);
+//		if (resp.get("data") != null) {
+//			BookEntity book = JSON.toJavaObject((JSON) resp.parse(resp.get("data").toString()), BookEntity.class);
+//			CommonUtil.printThree("获取JSONObject转成的实体对象：", book.toString());
+//		}
+//	}
+//
+//}
